7d41e1dabf4866ac553d0de467bdedf9c9e426c2
abirke <a.b.birke@student.tudelft.nl>
2024-05-17 22:43:58 +0200

    First setup. Navbar added.

---
+++ app/home/page.tsx
@@ -0,0 +1,15 @@
+import Nav from "../navigation/page"
+
+export default function Home() {
+  
+  return (
+    <>
+      <Nav/>
+      <div className="flex min-h-full flex-1 flex-col mt-10 lg:px-8 px-8 grid-flow-col auto-cols-max">
+        <div className="title-container sm:w-full sm:mx-auto sm:w-full sm:max-w-4xl mb-10">
+          hello 
+        </div>
+      </div>
+    </>
+  )
+}
